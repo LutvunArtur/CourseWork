e8b1743e4bc69c42d6853d60dca886fb2c228dc8
LutvunArtur <lutvun.artur1@gmail.com>
2023-04-02 09:04:57 +0300

    Не відпрацьовує ф-ції бай та селл

--- Work.py
+++ Work.py
@@ -6,7 +6,8 @@ json_data_file = "Data.json"
 
 def upd_json_file(some_data, file_name=json_data_file):
     with open(file_name, "w", encoding="utf-8") as my_data:
-        ress = json.dump(my_data, some_data)
+        result = json.dump(my_data, some_data)
+    return None
 
 
 with open(json_file, encoding="utf-8") as my_file:
@@ -15,27 +16,17 @@ with open(json_file, encoding="utf-8") as my_file:
 with open(json_data_file, encoding="utf-8") as data_file:
     data_res = json.load(data_file)
 
-buy_some = input()
-sell_some = input()
+
 trader = ArgumentParser()
-trader.add_argument("RATE", type=str, nargs="?")
-trader.add_argument("AVAILABLE", type=str, nargs="?")
-trader.add_argument("BUY", type=str, nargs="?")
-trader.add_argument("SELL", type=str, nargs="?")
-trader.add_argument("ALL", typr=str, nargs="?")
-trader.add_argument("NEXT", type=str, nargs="?")
-trader.add_argument("RESTART", type=str, nargs="?")
-trader.add_argument(buy_some, type=int, nargs="?")
-# trader.add_argument(, nargs="?")
+trader.add_argument("action", type=str, nargs="?")
+trader.add_argument("num", type=int, nargs="?")
 trader = vars(trader.parse_args())
 
 
 class Trade:
-    def __init__(self, data, ress):
-        self.buy_someth = buy_some
-        self.sell_someth = sell_some
+    def __init__(self, data, result):
         self.data_res = data
-        self.res = ress
+        self.res = result
         self.data_course = self.data_res["курс"]
         self.data_ua = self.data_res["на гривневому рахунку"]
         self.data_usd = self.data_res["на доларовому рахунку"]
@@ -46,6 +37,7 @@ class Trade:
         self.fst_delta = self.res["дельта"]
 
     def rate(self):
+        upd_json_file(self.res) if data_res == 0 else None
         return self.fst_course if data_res == 0 else self.data_course
 
     def available(self):
@@ -56,54 +48,55 @@ class Trade:
             self.fst_usd += self.fst_ua * self.fst_course
             result = self.fst_usd
         else:
-            self.data_ua += self.data_ua * self.data_course
+            self.data_usd += self.data_ua * self.data_course
             result = self.data_usd
         upd_json_file(result)
         return result
 
     def sell_all(self):
         if data_res == 0:
+            upd_json_file(res) if data_res == 0 else None
             self.fst_ua += self.fst_usd / self.fst_course
-            result = self.data_ua
+            result = self.fst_usd
         else:
             self.data_ua = self.data_usd / self.data_course
             result = self.data_ua
         upd_json_file(result)
         return result
 
-    def restart(self):
-        self.data_res = res
+    @staticmethod
+    def restart():
+        upd_json_file(res)
 
-    def buy_some(self):
-        result = None
+    def buy_some(self, some_num):
         if self.data_res == 0:
-            self.fst_ua += self.buy_someth * self.fst_course
-            if self.fst_usd< self.buy_someth:
+            upd_json_file(res)
+            self.fst_ua += some_num * self.fst_course
+            if self.fst_usd < some_num:
                 print("You dont have enough money")
                 pass
             result = self.fst_ua
         else:
-            self.data_ua += self.buy_someth * self.data_course
-            if self.data_ua < self.buy_someth:
+            self.data_ua += some_num * self.data_course
+            if self.data_ua < some_num:
                 print("You dont have enough money")
                 pass
             result = self.data_ua
         upd_json_file(result)
         return result
 
-    def sell_some(self):
-        result = None
+    def sell_some(self, some_num):
         if self.data_res == 0:
-            self.fst_ua += self.sell_someth * self.fst_course
-            self.fst_usd = self.fst_usd - self.sell_someth
-            if self.fst_usd < self.sell_someth:
+            upd_json_file(res)
+            self.fst_ua += some_num * self.fst_course
+            self.fst_usd = self.fst_usd - some_num
+            if self.fst_usd < some_num:
                 print("You dont have enough usd")
-                pass
             result = self.fst_ua, self.fst_usd
         else:
-            self.data_ua += self.sell_someth * self.data_course
-            self.data_usd = self.data_usd - self.sell_someth
-            if self.data_usd < self.sell_someth:
+            self.data_ua += some_num * self.data_course
+            self.data_usd = self.data_usd - some_num
+            if self.data_usd < some_num:
                 print("You dont have enough usd")
                 pass
             result = self.data_ua,  self.data_usd
@@ -112,22 +105,26 @@ class Trade:
 
 
 trader_ex = Trade(data_res, res)
-if trader["RATE"]:
+if trader["action"] == "RATE":
     print(round(trader_ex.rate(), 2))
-elif trader["AVAILABLE"]:
+elif trader["action"] == "AVAILABLE":
     print(trader_ex.available())
-elif trader["BUY"]:
-    if trader["ALL"]:
+elif trader["action"] == "BUY":
+    if trader["action"] == "ALL":
         trader_ex.buy_all()
-        print(round(trader_ex.buy_all(), 2))
-    elif trader[buy_some] == int(input()):
-        trader_ex.buy_some()
-        print(round(trader_ex.buy_some(), 2))
-elif trader["SELL"]:
-    if trader["ALL"]:
+        print("done")
+    elif trader["action"] == trader["num"]:
+        trader_ex.buy_some(trader["num"])
+        print("DONE")
+elif trader["action"] == "SELL":
+    if trader["action"] == "ALL":
         trader_ex.sell_all()
-        print(trader_ex.sell_all())
-    elif trader[sell_some]:
-        trader_ex.buy_some()
-        print(trader_ex.buy_some())
-
+        print("DONE")
+    elif trader["action"] == trader["num"]:
+        trader_ex.buy_some(trader["num"])
+        print("DONE")
+elif trader["action"] == "RESTART":
+    trader_ex.restart()
+    print("done!")
+else:
+    print("Wrong request, try again")
