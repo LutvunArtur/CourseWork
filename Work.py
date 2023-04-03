b2d7d4d193b54b479be131e38e2fdc2f5fa2c26d
LutvunArtur <lutvun.artur1@gmail.com>
2023-04-03 14:39:20 +0300

    Міні тредингова программа, яка показує актуальний баланс та курс валют в гривнях і долларах

--- Work.py
+++ Work.py
@@ -1,13 +1,13 @@
 from argparse import ArgumentParser
 import json
+from random import randint
 json_file = "work.json"
-json_data_file = "Data.json"
+json_data_file = "data.json"
 
 
 def upd_json_file(some_data, file_name=json_data_file):
     with open(file_name, "w", encoding="utf-8") as my_data:
-        result = json.dump(my_data, some_data)
-    return None
+        result = json.dump(some_data, my_data)
 
 
 with open(json_file, encoding="utf-8") as my_file:
@@ -19,7 +19,7 @@ with open(json_data_file, encoding="utf-8") as data_file:
 
 trader = ArgumentParser()
 trader.add_argument("action", type=str, nargs="?")
-trader.add_argument("num", type=int, nargs="?")
+trader.add_argument("num", type=str, nargs="?")
 trader = vars(trader.parse_args())
 
 
@@ -27,18 +27,17 @@ class Trade:
     def __init__(self, data, result):
         self.data_res = data
         self.res = result
-        self.data_course = self.data_res["курс"]
-        self.data_ua = self.data_res["на гривневому рахунку"]
-        self.data_usd = self.data_res["на доларовому рахунку"]
-        self.data_delta = self.data_res["дельта"]
-        self.fst_course = self.res["курс"]
-        self.fst_ua = self.res["на гривневому рахунку"]
-        self.fst_usd = self.res["на доларовому рахунку"]
-        self.fst_delta = self.res["дельта"]
+        self.data_course = self.data_res["course"]
+        self.data_ua = self.data_res["UA"]
+        self.data_usd = self.data_res["USD"]
+        self.data_delta = self.data_res["delta"]
+        self.fst_course = self.res["course"]
+        self.fst_ua = self.res["UA"]
+        self.fst_usd = self.res["USD"]
+        self.fst_delta = self.res["delta"]
 
     def rate(self):
-        upd_json_file(self.res) if data_res == 0 else None
-        return self.fst_course if data_res == 0 else self.data_course
+        return round(self.fst_course, 2) if data_res == 0 else round(self.data_course, 2)
 
     def available(self):
         return f"{round(self.fst_ua, 2)} UA, {round(self.fst_usd, 2)} USD" if data_res == 0 else \
@@ -46,52 +45,112 @@ class Trade:
 
     def buy_all(self):
         if data_res == 0:
-            self.fst_usd += self.fst_ua * self.fst_course
-            result = self.fst_usd
+            res_dict = {}
+            self.fst_usd += self.fst_ua / self.fst_course
+            self.fst_ua = 0
+            res_dict["UA"] = round(self.fst_ua, 2)
+            res_dict["USD"] = round(self.fst_usd, 2)
+            res_dict["course"] = round(self.fst_course, 2)
+            res_dict["delta"] = round(self.fst_delta, 2)
         else:
-            self.data_usd += self.data_ua * self.data_course
-            result = self.data_usd
-        upd_json_file(result)
-        return None
+            res_dict = {}
+            self.data_usd += self.data_ua / self.data_course
+            self.data_ua = 0
+            res_dict["UA"] = round(self.data_ua, 2)
+            res_dict["USD"] = round(self.data_usd, 2)
+            res_dict["course"] = round(self.data_course, 2)
+            res_dict["delta"] = round(self.data_delta, 2)
+        return res_dict
 
     def sell_all(self):
         if data_res == 0:
-            upd_json_file(res) if data_res == 0 else None
-            self.fst_ua += self.fst_usd / self.fst_course
-            result = self.fst_usd
+            res_dict = {}
+            self.fst_ua += self.fst_usd * self.fst_course
+            self.fst_usd = 0
+            res_dict["UA"] = round(self.fst_ua, 2)
+            res_dict["USD"] = round(self.fst_usd, 2)
+            res_dict["course"] = round(self.fst_course, 2)
+            res_dict["delta"] = round(self.fst_delta, 2)
         else:
-            self.data_ua = self.data_usd / self.data_course
-            result = self.data_ua
-        upd_json_file(result)
-        return None
+            res_dict = {}
+            self.data_ua += self.data_usd * self.data_course
+            self.data_usd = 0
+            res_dict["UA"] = round(self.data_ua, 2)
+            res_dict["USD"] = round(self.data_usd, 2)
+            res_dict["course"] = round(self.data_course, 2)
+            res_dict["delta"] = round(self.data_delta, 2)
+        return res_dict
 
     @staticmethod
     def restart():
         upd_json_file(res)
 
     def buy_some(self, some_num):
-        result = self.data_ua
         if self.data_res == 0:
+            res_dict = {}
+            if self.fst_ua < some_num:
+                raise Exception("You dont have enough money")
             upd_json_file(res)
-            self.data_ua += some_num * self.data_course
+            self.fst_usd += some_num
+            res_ua = self.fst_ua - (some_num * self.data_course)
+            res_dict["UA"] = round(res_ua, 2)
+            res_dict["USD"] = round(self.fst_usd, 2)
+            res_dict["course"] = round(self.fst_course, 2)
+            res_dict["delta"] = round(self.fst_delta, 2)
+        else:
+            res_dict = {}
             if self.data_ua < some_num:
-                print("You dont have enough money")
-            result = self.data_ua
-        upd_json_file(result)
-        return None
+                raise Exception("You dont have enough money")
+            self.data_usd += some_num
+            res_ua = self.data_ua - (some_num * self.data_course)
+            res_dict["UA"] = round(res_ua, 2)
+            res_dict["USD"] = round(self.data_usd, 2)
+            res_dict["course"] = round(self.data_course, 2)
+            res_dict["delta"] = round(self.data_delta, 2)
+        return res_dict
 
     def sell_some(self, some_num):
-        result = self.data_ua, self.data_usd
         if self.data_res == 0:
-            upd_json_file(res)
+            res_dict = {}
+            if self.fst_usd < some_num:
+                raise Exception("You dont have enough usd")
+            self.fst_ua += some_num * self.fst_course
+            self.fst_usd = self.fst_usd - some_num
+            res_dict["UA"] = round(self.fst_ua, 2)
+            res_dict["USD"] = round(self.fst_usd, 2)
+            res_dict["course"] = round(self.fst_course, 2)
+            res_dict["delta"] = round(self.fst_delta, 2)
+        else:
+            res_dict = {}
+            if self.data_usd < some_num:
+                raise Exception("You dont have enough usd")
             self.data_ua += some_num * self.data_course
             self.data_usd = self.data_usd - some_num
-            if self.data_usd < some_num:
-                print("You dont have enough usd")
-                pass
-            result = self.data_ua,  self.data_usd
-        upd_json_file(result)
-        return None
+            res_dict["UA"] = round(self.data_ua, 2)
+            res_dict["USD"] = round(self.data_usd, 2)
+            res_dict["course"] = round(self.data_course, 2)
+            res_dict["delta"] = round(self.data_delta, 2)
+        return res_dict
+
+    def next(self):
+        rand_num = randint(1, 10)
+        if self.data_res == 0:
+            res_dict = {}
+            res_course = self.fst_course + self.fst_delta if rand_num < 5\
+                else self.fst_course - self.fst_delta
+            res_dict["UA"] = round(self.fst_ua, 2)
+            res_dict["USD"] = round(self.fst_usd, 2)
+            res_dict["course"] = round(res_course, 2)
+            res_dict["delta"] = round(self.fst_delta, 2)
+        else:
+            res_dict = {}
+            res_course = self.data_course + self.data_delta if rand_num < 5 \
+                else self.data_course - self.data_delta
+            res_dict["UA"] = round(self.data_ua, 2)
+            res_dict["USD"] = round(self.data_usd, 2)
+            res_dict["course"] = round(res_course, 2)
+            res_dict["delta"] = round(self.data_delta, 2)
+        return res_dict
 
 
 trader_ex = Trade(data_res, res)
@@ -100,21 +159,24 @@ if trader["action"] == "RATE":
 elif trader["action"] == "AVAILABLE":
     print(trader_ex.available())
 elif trader["action"] == "BUY":
-    if trader["action"] == "ALL":
-        trader_ex.buy_all()
+    if trader["num"] == "ALL":
+        upd_json_file(trader_ex.buy_all())
         print("done")
-    elif trader["num"]:
-        trader_ex.buy_some(trader["num"])
-        print("DONE")
+    elif trader["num"] == int:
+        upd_json_file(trader_ex.buy_some(trader["num"]))
+        print("done!")
 elif trader["action"] == "SELL":
-    if trader["action"] == "ALL":
-        trader_ex.sell_all()
-        print("DONE")
-    elif trader["num"]:
-        trader_ex.buy_some(trader["num"])
-        print("DONE")
+    if trader["num"] == "ALL":
+        upd_json_file(trader_ex.sell_all())
+        print("done!")
+    elif trader["num"] == int:
+        upd_json_file(trader_ex.sell_some(trader["num"]))
+        print("done!")
 elif trader["action"] == "RESTART":
     trader_ex.restart()
+    print("done!")
+elif trader["action"] == "NEXT":
+    upd_json_file(trader_ex.next())
     print("done!")
 else:
     print("Wrong request, try again")
